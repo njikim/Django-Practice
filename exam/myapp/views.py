9afff054bd0d8ff1dc7b74dbd166a318d196371f
njikim <eunjikim214@gmail.com>
2019-04-25 22:35:56 +0900

    delete

--- exam/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse
-# Create your views here.
-
-def index(request):     
-    return HttpResponse("안녕하세요")
-
-def hello_template(request):
-    return render(request, 'hello.html')
